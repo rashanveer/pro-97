59638b0777f864908bb21f8d11e57ab5f5005011
rashanveer <73472412+rashanveer@users.noreply.github.com>
2022-07-24 20:50:34 +0530

    Add files via upload

---
+++ guessingGame.py
@@ -0,0 +1,9 @@
+import random
+number = random.randint(1,9)
+while number < 5:
+    if number == number:
+        print("conratulation YOU WON!!")
+        break
+    if not number < 5:
+        print("You lose !! The number is",number)
+
